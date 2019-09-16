72ffa19a7f5f2ed0ae33ca459a4e5ec421c39de5
abhishekkumarpandey <abhisonu3629@gmail.com>
2019-09-16 11:53:52 +0530

    OrderController Sepcific Changes for /saveOrder & /getAllPastOrders

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/OrdersBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/OrdersBusinessService.java
@@ -95,16 +95,16 @@ public class OrdersBusinessService {
   public List<OrdersEntity> getAllPastOrders(String authorization) {
 
 //    Not Needed as such becuase we have UUID for Customer we can directly query Orders Table
-    CustomerEntity customerEntity = customerDao.getCustomerById("7d174a25-ba31-45a8-85b4-b06ffc9d5f8f");
+    CustomerEntity customerEntity = customerDao.getCustomerById("1c40d816-082b-4660-96a3-a3aa36fd97a0");
 
-    List<OrdersEntity> ordersEntity = ordersDao.getOrdersByCustomerUuid(1);
+    List<OrdersEntity> ordersEntity = ordersDao.getOrdersByCustomerUuid(customerEntity);
 
     return ordersEntity;
   }
 
   @Transactional(propagation = Propagation.REQUIRED)
-  public List<OrderItemEntity> getOrderItemFromOrder(String orderUuid) {
-    List<OrderItemEntity> orderItemEntities = orderItemDao.getOrderItemByOrder(orderUuid);
+  public List<OrderItemEntity> getOrderItemFromOrder(OrdersEntity order) {
+    List<OrderItemEntity> orderItemEntities = orderItemDao.getOrderItemByOrder(order);
     return orderItemEntities;
   }
 }
